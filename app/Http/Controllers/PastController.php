461244bd03353ed6cc75d8ee82c72d577cb09e57
lobotomised <romain@rprevost.fr>
2019-07-02 15:12:02 +0200

    Extract store to api controller

--- app/Http/Controllers/PastController.php
+++ app/Http/Controllers/PastController.php
@@ -2,25 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\CreatePastRequest;
 use App\Models\Past;
-use App\Repositories\PastRepository;
-use Illuminate\Http\JsonResponse;
 use Illuminate\View\View;
-use function now;
 
 class PastController extends Controller
 {
-
-    /**
-     * @var \App\Repositories\PastRepository
-     */
-    private $pastRepository;
-
-    public function __construct(PastRepository $pastRepository)
-    {
-        $this->pastRepository = $pastRepository;
-    }
 
     /**
      * @return \Illuminate\View\View
@@ -31,52 +17,12 @@ class PastController extends Controller
     }
 
     /**
-     * @param \App\Http\Requests\CreatePastRequest $request
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function store(CreatePastRequest $request): JsonResponse
-    {
-        switch ($request->encrypted) {
-            case '5m':
-                $expire = now()->addMinutes(5);
-                break;
-            case '1h':
-                $expire = now()->addHour();
-                break;
-            case '1d':
-                $expire = now()->addDay();
-                break;
-            case '1w':
-                $expire = now()->addWeek();
-                break;
-            case '1m':
-                $expire = now()->addMonth();
-                break;
-            case '1y':
-                $expire = now()->addYear();
-                break;
-            default:
-                $expire = now()->addWeek();
-                break;
-        }
-
-        $past = new Past;
-        $past->encrypted = $request->encrypted;
-        $past->expire_at = $expire;
-
-        $past->save();
-
-        return response()->json($past->id);
-    }
-
-    /**
      * @param \App\Models\Past $past
      *
      * @return \Illuminate\View\View
      */
     public function view(Past $past): View
     {
-        return view('past.view');
+        return view('past.view')->with(compact('past'));
     }
 }
