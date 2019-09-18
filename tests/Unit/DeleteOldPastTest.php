b2e38d246f057fcfc1f764b99d9b125bf1bc499d
romai <romain@rprevost.fr>
2019-09-19 00:41:43 +0200

    Add unit test for delete old action

---
+++ tests/Unit/DeleteOldPastTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Actions\DeleteExpiredPastAction;
+use App\Events\PastCreated;
+use App\Models\Past;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Event;
+use Tests\TestCase;
+
+class DeleteOldPastTest extends TestCase
+{
+    /**
+     * @var \App\Actions\DeleteExpiredPastAction
+     */
+    private $deleteExpiredPastAction;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->deleteExpiredPastAction = $this->app->make(DeleteExpiredPastAction::class);
+    }
+
+    /**
+     * @test
+     */
+    public function expired_past_should_be_deleted()
+    {
+        Event::fake([
+            PastCreated::class,
+        ]);
+
+        $past = factory(Past::class)->create([
+            'expire_at' => Carbon::now()->subMonth(),
+        ]);
+
+        $this->deleteExpiredPastAction->delete();
+
+        $this->assertDatabaseMissing('Pasts', [
+            'id' => $past->id,
+        ]);
+    }
+}
