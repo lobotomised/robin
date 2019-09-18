bdedfb252d03f8d57eaec8343719a1ad4644f09d
romai <romain@rprevost.fr>
2019-09-19 01:53:29 +0200

    fix style ci

--- app/Actions/DeleteExpiredPastAction.php
+++ app/Actions/DeleteExpiredPastAction.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Actions;
 
 use App\Models\Past;
-use Carbon\Carbon;
 use Psr\Log\LoggerInterface;
 
 class DeleteExpiredPastAction
--- tests/Unit/DeleteOldPastTest.php
+++ tests/Unit/DeleteOldPastTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Unit;
 
 use App\Actions\DeleteExpiredPastAction;
@@ -25,7 +27,7 @@ class DeleteOldPastTest extends TestCase
     /**
      * @test
      */
-    public function expired_past_should_be_deleted()
+    public function expired_past_should_be_deleted(): void
     {
         Event::fake([
             PastCreated::class,
