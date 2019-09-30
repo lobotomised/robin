76262c74f58d902587b31f19daca5337e91c49af
romai <romain@rprevost.fr>
2019-09-30 19:20:39 +0200

    use view helper

--- app/Http/Controllers/PastController.php
+++ app/Http/Controllers/PastController.php
@@ -5,32 +5,16 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Models\Past;
-use Illuminate\Contracts\View\Factory;
 use Illuminate\View\View;
 
 final class PastController extends Controller
 {
     /**
-     * @var \Illuminate\Contracts\View\Factory
-     */
-    private $view;
-
-    /**
-     * PastController constructor.
-     *
-     * @param \Illuminate\Contracts\View\Factory $view
-     */
-    public function __construct(Factory $view)
-    {
-        $this->view = $view;
-    }
-
-    /**
      * @return \Illuminate\View\View
      */
     public function create(): View
     {
-        return $this->view->make('past.create');
+        return view('past.create');
     }
 
     /**
@@ -40,6 +24,6 @@ final class PastController extends Controller
      */
     public function view(Past $past): View
     {
-        return $this->view->make('past.view')->with(compact('past'));
+        return view('past.view')->with(compact('past'));
     }
 }
