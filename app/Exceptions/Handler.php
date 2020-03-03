f66329b4a8220e7396269661f03e930dec13f702
lobotomised <romain@rprevost.fr>
2020-03-03 15:49:33 +0100

    upgrade laravel.7

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace App\Exceptions;
 
-use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
 
 class Handler extends ExceptionHandler
 {
@@ -31,12 +31,12 @@ class Handler extends ExceptionHandler
     /**
      * Report or log an exception.
      *
-     * @param  \Exception  $exception
+     * @param  \Throwable  $exception
      *
      * @return void
      * @throws \Exception
      */
-    public function report(Exception $exception): void
+    public function report(Throwable $exception): void
     {
         if ($this->shouldReport($exception) && app()->isProduction() && app()->bound('sentry')) {
             app('sentry')->captureException($exception);
@@ -49,11 +49,12 @@ class Handler extends ExceptionHandler
      * Render an exception into an HTTP response.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
+     * @param  \Throwable  $exception
+     * @return \Symfony\Component\HttpFoundation\Response
      *
-     * @return \Illuminate\Http\Response
+     * @throws \Throwable
      */
-    public function render($request, Exception $exception)
+    public function render($request, Throwable $exception)
     {
         return parent::render($request, $exception);
     }
